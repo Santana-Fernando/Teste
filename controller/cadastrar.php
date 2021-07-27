96fd73d4ee948fc0098a76deeed62098fee2c60b
Fernando Rodrigues de Santana <fernando@avaloon.com.br>
2021-07-26 22:27:02 -0300

    Adicionando cadastro

--- controller/cadastrar.php
+++ controller/cadastrar.php
@@ -17,6 +17,24 @@
     $destino = $_POST['destino'];
     $observacoes = $_POST['observacoes'];
 
+    
+    echo "\n $data";
+    echo "\n $hora_inicial";
+    echo "\n $hora_final";
+    echo "\n $turno";
+    echo "\n $motorista";
+    echo "\n $requisicao";
+    echo "\n $material";
+    echo "\n $peso_entrada";
+    echo "\n $peso_saida";
+    echo "\n $diferenca_de_peso";
+    echo "\n $tag";
+    echo "\n $KM";
+    echo "\n $origem";
+    echo "\n $destino";
+    echo "\n $observacoes";
+        
+
     $registros = "INSERT INTO registros (data, hora_inicial, hora_final, turno, motorista, requisicao, material, peso_entrada, peso_saida, diferenca_de_peso, tag, KM, origem , destino, observacoes) VALUES ('$data','$hora_inicial','$hora_final','$turno','$motorista','$requisicao','$material','$peso_entrada','$peso_saida','$diferenca_de_peso','$tag','$KM','$origem','$destino','$observacoes')";
 
     $resultado = mysqli_query($conn, $registros);
@@ -24,6 +42,6 @@
     if($resultado){
         echo "Cadastro feito com sucesso!!";
     } else {
-        echo "Erro ao cadastrar $resultado";
+        echo "Erro ao cadastrar $registros";
     }
 ?>
